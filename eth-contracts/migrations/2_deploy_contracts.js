e30805da35ac6ef449073a5569e59045c237d9a8
NAIF <naifalsaleh3000@gmail.com>
2019-04-06 13:13:02 +0300

    add Sequare Verifier Contract

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 // migrating the appropriate contracts
-// var SquareVerifier = artifacts.require("./SquareVerifier.sol");
+var SquareVerifier = artifacts.require("./SquareVerifier.sol");
 var ERC721Mintable = artifacts.require("./ERC721Mintable.sol")
-SolnSquareVerifier = artifacts.require("SolnSquareVerifier");
+var SolnSquareVerifier = artifacts.require("/SolnSquareVerifier.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ERC721Mintable);
-  // deployer.deploy(SquareVerifier);
+  deployer.deploy(SquareVerifier);
   deployer.deploy(SolnSquareVerifier);
 };
