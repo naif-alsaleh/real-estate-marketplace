f8dbdbce2df2a1f464fd160fbb00fe16b0b2d986
jal <jaldemad@gmail.com>
2019-04-08 18:57:51 +0300

    compine the square verifier with ERC721 token

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,34 +1,74 @@
 pragma solidity >=0.4.21 <0.6.0;
 
+import "./ERC721Mintable.sol";
+import "./SquareVerifier.sol";
+
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
-contract SolnSquareVerifier {
-
-
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is ERC721Mintable {
 
+    SquareVerifier private verifier;
 
+    constructor() public {
+        verifier = new SquareVerifier();
+    }
 
-// TODO define a solutions struct that can hold an index & an address
+    // TODO define a solutions struct that can hold an index & an address
+    struct Solution {
+        uint index;
+        address addr;
+    }
 
+    // TODO define an array of the above struct
+    Solution[] private solutions;
+    uint private solutionCount = 0;
 
-// TODO define an array of the above struct
+    // TODO define a mapping to store unique solutions submitted
+    mapping(uint => mapping(uint => bool)) private uniqueSolutions;
 
+    // TODO Create an event to emit when a solution is added
+    event SolutionAdded(
+        uint a,
+        uint b
+    );
 
-// TODO define a mapping to store unique solutions submitted
+    // TODO Create a function to add the solutions to the array and emit the event
+    function _addSolution(uint first, uint second) internal {
+        solutions.push(Solution({
+            index: solutionCount++,
+            addr: msg.sender
+        }));
+        emit SolutionAdded(first, second);
+    }
 
+    // TODO Create a function to mint new NFT only after the solution has been verified
+    //  - make sure the solution is unique (has not been used before)
+    //  - make sure you handle metadata as well as tokenSuplly
+    function mint(
+        uint[2] calldata a,
+        uint[2] calldata a_p,
+        uint[2][2] calldata b,
+        uint[2] calldata b_p,
+        uint[2] calldata c,
+        uint[2] calldata c_p,
+        uint[2] calldata h,
+        uint[2] calldata k,
+        uint[2] calldata input,
+        address to, 
+        uint256 tokenId
+    ) external onlyOwner() returns(bool) {
+        require(!uniqueSolutions[input[0]][input[1]], "This proof has been submitted before");
+        
+        bool result = verifier.verifyTx(a, a_p, b, b_p, c, c_p, h, k, input);
+        require(result, "you have provide an incorrect proof");
 
+        super._mint(to, tokenId);
+        super._setTokenURI(tokenId);
 
-// TODO Create an event to emit when a solution is added
-
-
-
-// TODO Create a function to add the solutions to the array and emit the event
-
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
+        uniqueSolutions[input[0]][input[1]] = true;
+        _addSolution(input[0], input[1]);
+        return true;
+    }
 }
   
 
