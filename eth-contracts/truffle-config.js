1ebdf36de361708dadccc09e29664cd9118a7808
jal <jaldemad@gmail.com>
2019-04-09 19:28:36 +0300

    add rinkeby nework configuration

--- eth-contracts/truffle-config.js
+++ eth-contracts/truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -47,6 +47,15 @@ module.exports = {
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
      },
+
+     rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/c0c2ccd635d44b6cb9da57c72299cabc")
+      },
+      network_id: '4',
+      gas: 6036017,
+      gasPrice: 10000000000,
+    }
 
     // Another network with more advanced options...
     // advanced: {
